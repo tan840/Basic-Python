08837b68269c2ccb8346763f0b375f1b54c712a4
Tanvir Ahmed <tan.ahmed98@gmail.com>
2019-10-11 23:50:57 +0600

    Basic functions and conditions

---
+++ PythonScript.py
@@ -0,0 +1,32 @@
+print("Hello World")
+x = 20;
+y = 2.5;
+
+def addten(param1):
+	#do Staff
+	if type(param1) != int:
+		print("Not an Integer")
+	else:
+		print("Integer found")
+		return param1 +10
+
+x=addten(x)
+
+y=addten(y)
+print(x,y)
+
+a=30
+b=4.5
+
+
+def subThree(param2):
+	#do otherthing
+	if type(param2) !=int :
+		print("not an Integer")
+	else:
+		print("Integer found")
+		return param2-3
+
+a=subThree(a)
+b=subThree(b)
+print(a,b)
